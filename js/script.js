b715a5facde411232085e43649d8d53bb3d9b4df
Pedro Nascimento <157742643+PedroNascimento009@users.noreply.github.com>
2025-03-24 19:37:41 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// IMG Reveal Scroll
+document.addEventListener("scroll", function () {
+    const pratos = document.querySelectorAll(".prato");
+    const windowHeight = window.innerHeight;
+    
+    pratos.forEach(prato => {
+        const img = prato.querySelector("img");
+        const rect = img.getBoundingClientRect();
+        
+        if (rect.top > windowHeight / 4 && rect.bottom < windowHeight * 3.5 / 4.5) {
+            prato.classList.add("visible");
+        } else {
+            prato.classList.remove("visible");
+        }
+    });
+});
