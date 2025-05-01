5dd6b36095d32d2ad540995806a4f0f88d467901
Pedro Nascimento <157742643+PedroNascimento009@users.noreply.github.com>
2025-05-01 14:46:32 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -24,4 +24,19 @@ function abrirTabs(IdTab) {
     }
 
     document.getElementById(IdTab).style.display = "block";
+    document.getElementById('intro').style.display = "none";
 }
+
+//TROCA DE IMGS
+const figuras = [document.getElementById("iamgem1"), document.getElementById("imagem2")];
+let indice = 0;
+
+setInterval(() => {
+  const atual = figuras[indice];
+  const proxima = figuras[(indice + 1) % figuras.length];
+
+  atual.classList.remove("visivel");
+  proxima.classList.add("visivel");
+
+  indice = (indice + 1) % figuras.length;
+}, 4000);
