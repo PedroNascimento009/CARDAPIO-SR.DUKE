bf732e93d34f90557e1cec60d04b949a3cbbe334
Pedro Nascimento <157742643+PedroNascimento009@users.noreply.github.com>
2025-05-17 14:04:57 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
-//INTRO
-setTimeout(() => {
-    document.getElementById("introdution").classList.add("visivel");
-}, 1000);
-
 // IMG Reveal Scroll
-document.addEventListener("scroll", function () {
-    const dish = document.querySelectorAll(".cont-dish");
+function abrirTabs(id) {
+    document.querySelectorAll('.main-category').forEach(sec => {
+        sec.style.display = 'none';
+    });
+
+    const secAtual = document.getElementById(id);
+    secAtual.style.display = 'block';
+
+    verificarVisibilidade();
+}
+
+function verificarVisibilidade() {
+    const dishes = document.querySelectorAll(".category-dish-item");
     const windowHeight = window.innerHeight;
 
-    dish.forEach(dish => {
+    dishes.forEach(dish => {
         const img = dish.querySelector("img");
+        if (!img) return;
+
         const rect = img.getBoundingClientRect();
 
         if (rect.top > windowHeight / 5 && rect.bottom < windowHeight * 3.8 / 4.7) {
@@ -18,10 +26,15 @@ document.addEventListener("scroll", function () {
             dish.classList.remove("visible");
         }
     });
-});
+}
+
+document.addEventListener("scroll", verificarVisibilidade);
+
+document.addEventListener("DOMContentLoaded", verificarVisibilidade);
+
 
 //TABS NAVBAR
-function abrirTabs(IdTab) {
+function openTabs(IdTab) {
     var category = document.getElementsByClassName('main-category');
 
     for (var i = 0; i < category.length; i++) {
@@ -29,7 +42,6 @@ function abrirTabs(IdTab) {
     }
 
     document.getElementById(IdTab).style.display = "block";
-    document.getElementById('intro').style.display = "none";
 }
 
 //TROCA DE IMGS
