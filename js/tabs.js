a382df168db9f092ec3a4ee7340155f3cad888ff
Pedro Nascimento <157742643+PedroNascimento009@users.noreply.github.com>
2025-05-22 17:32:25 -0300

    tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -1,4 +1,17 @@
-function openTabs(IdTab) {
+import { verificarVisibilidade } from './scrollReveal.js';
+
+export function abrirTabs(id) {
+    document.querySelectorAll('.main-category').forEach(sec => {
+        sec.style.display = 'none';
+    });
+
+    const secAtual = document.getElementById(id);
+    secAtual.style.display = 'block';
+
+    verificarVisibilidade();
+}
+
+export function openTabs(IdTab) {
     var category = document.getElementsByClassName('main-category');
 
     for (var i = 0; i < category.length; i++) {
