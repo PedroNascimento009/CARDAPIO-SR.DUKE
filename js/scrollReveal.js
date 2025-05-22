4206bce435647d62e5dc185ef2a384289dcf1ad7
Pedro Nascimento <157742643+PedroNascimento009@users.noreply.github.com>
2025-05-22 17:25:36 -0300

    scrollReveal.js

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,12 +1,32 @@
-const figuras = [document.getElementById("imagem1"), document.getElementById("imagem2")];
-let indice = 0;
+function abrirTabs(id) {
+    document.querySelectorAll('.main-category').forEach(sec => {
+        sec.style.display = 'none';
+    });
 
-setInterval(() => {
-    const atual = figuras[indice];
-    const proxima = figuras[(indice + 1) % figuras.length];
+    const secAtual = document.getElementById(id);
+    secAtual.style.display = 'block';
 
-    atual.classList.remove("visivel");
-    proxima.classList.add("visivel");
+    verificarVisibilidade();
+}
 
-    indice = (indice + 1) % figuras.length;
-}, 4000);
+function verificarVisibilidade() {
+    const dishes = document.querySelectorAll(".category-dish-item, .cocktail-item");
+    const windowHeight = window.innerHeight;
+
+    dishes.forEach(dish => {
+        const img = dish.querySelector("img");
+        if (!img) return;
+
+        const rect = img.getBoundingClientRect();
+
+        if (rect.top > windowHeight / 5 && rect.bottom < windowHeight * 3.8 / 4.7) {
+            dish.classList.add("visible");
+        } else {
+            dish.classList.remove("visible");
+        }
+    });
+}
+
+document.addEventListener("scroll", verificarVisibilidade);
+
+document.addEventListener("DOMContentLoaded", verificarVisibilidade);
