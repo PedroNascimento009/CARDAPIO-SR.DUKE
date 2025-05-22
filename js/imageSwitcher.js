0132fac8941e579da465e2a8c2eb6953617e1b0f
Pedro Nascimento <157742643+PedroNascimento009@users.noreply.github.com>
2025-05-22 17:26:22 -0300

    imageSwitcher.js

--- js/imageSwitcher.js
+++ js/imageSwitcher.js
@@ -1,32 +1,12 @@
-function abrirTabs(id) {
-    document.querySelectorAll('.main-category').forEach(sec => {
-        sec.style.display = 'none';
-    });
+const figuras = [document.getElementById("imagem1"), document.getElementById("imagem2")];
+let indice = 0;
 
-    const secAtual = document.getElementById(id);
-    secAtual.style.display = 'block';
+setInterval(() => {
+    const atual = figuras[indice];
+    const proxima = figuras[(indice + 1) % figuras.length];
 
-    verificarVisibilidade();
-}
+    atual.classList.remove("visivel");
+    proxima.classList.add("visivel");
 
-function verificarVisibilidade() {
-    const dishes = document.querySelectorAll(".category-dish-item, .cocktail-item");
-    const windowHeight = window.innerHeight;
-
-    dishes.forEach(dish => {
-        const img = dish.querySelector("img");
-        if (!img) return;
-
-        const rect = img.getBoundingClientRect();
-
-        if (rect.top > windowHeight / 5 && rect.bottom < windowHeight * 3.8 / 4.7) {
-            dish.classList.add("visible");
-        } else {
-            dish.classList.remove("visible");
-        }
-    });
-}
-
-document.addEventListener("scroll", verificarVisibilidade);
-
-document.addEventListener("DOMContentLoaded", verificarVisibilidade);
+    indice = (indice + 1) % figuras.length;
+}, 4000);
