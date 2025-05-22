78a2a0c39cb453cdf86ad7c237dea5e2886745a5
Pedro Nascimento <157742643+PedroNascimento009@users.noreply.github.com>
2025-05-22 17:24:50 -0300

    scrollReveal.js

---
+++ js/scrollReveal.js
@@ -0,0 +1,12 @@
+const figuras = [document.getElementById("imagem1"), document.getElementById("imagem2")];
+let indice = 0;
+
+setInterval(() => {
+    const atual = figuras[indice];
+    const proxima = figuras[(indice + 1) % figuras.length];
+
+    atual.classList.remove("visivel");
+    proxima.classList.add("visivel");
+
+    indice = (indice + 1) % figuras.length;
+}, 4000);
