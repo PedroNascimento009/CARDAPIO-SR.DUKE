2bcff4b3e1dc67bf0ee8d9ca123c7f4952fbc9c8
Pedro Nascimento <pedro.nascimento00099@gmail.com>
2025-10-18 16:48:50 -0300

    correção no promotion

--- js/utils/tabs.js
+++ js/utils/tabs.js
@@ -1,8 +1,9 @@
+import { exibirPratoDaSemana } from './weekDish.js';
+
 export function tabs() {
   document.querySelectorAll('.category-button').forEach(botao => {
     botao.addEventListener('click', () => {
       const categoria = botao.dataset.category
-      console.log("Botão clicado! Categoria:")
       pegarDados(categoria)
     })
   })
@@ -13,6 +14,10 @@ async function pegarDados(aba) {
     const resp = await fetch(`/assets/pages/categories/${aba}.html`)
     const dados = await resp.text()
     document.querySelector('#category-container').innerHTML = dados
+  
+    if (aba === 'promotions') {
+      exibirPratoDaSemana();
+    }
   } catch {
     console.error("Erro ao buscar aba:", error);
     document.querySelector('#category-container').innerHTML = "Aba não encontrada"
