177408ddbeb73b9ca62f8140f51007efbc5b2146
Pedro Nascimento <pedro.nascimento00099@gmail.com>
2025-11-13 15:48:47 -0300

    remoção da aba promoção

--- js/global.js
+++ js/global.js
@@ -1,9 +1,7 @@
 import { iniciarScrollReveal } from './utils/scrollReveal.js';
 import { iniciarTrocaDeImagens } from './utils/imageSwitcher.js';
-import { exibirPratoDaSemana } from './utils/weekDish.js';
 import { tabs } from './utils/tabs.js';
 
 iniciarScrollReveal();
 iniciarTrocaDeImagens();
-exibirPratoDaSemana();
 tabs();
